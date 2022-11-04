8d13c55b77ecffdf3f845841bbbc34f5204c8c27
lucas-alencar <lucasalencar.rd@gmail.com>
2022-11-04 15:53:50 -0300

    Starting ClientRepository

--- src/main/java/io/github/lucasalencar/repository/ClientRepository.java
+++ src/main/java/io/github/lucasalencar/repository/ClientRepository.java
@@ -3,5 +3,5 @@ package io.github.lucasalencar.repository;
 import io.github.lucasalencar.model.ClientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository{
+public interface ClientRepository extends JpaRepository<ClientModel,Integer>{
 }
